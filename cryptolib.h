012a1265883f3f037397e0d2c4deb6ca2ba0fb65
Shankjbs571 <Shankjbs571@gmail.com>
2024-06-19 00:28:22 +0530

    init

--- cryptolib.h
+++ cryptolib.h
@@ -1,2 +1,24 @@
-/* File: arcfour.h
+/* File: cryptolib.h
  author: Shashank */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define rc4decrypt(x,y) rc4encrypt(x,y)
+
+struct s_arcfour {
+    // ...
+};
+
+typedef struct s_arcfour Arcfour;
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+
+Arcfour *rc4init(int8*, int16);
+int8 rc4byte(void);
+int8 *rc4encrypt(int8*, int16);
--- example.c
+++ example.c
@@ -1,2 +1,28 @@
 /* This is for testing only */
 
+#include "cryptolib.h"
+
+#define F fflush(stdout)
+
+
+int main(){
+    Arcfour *rc4;
+    char *key, *text, *encrypted, *decrypted;
+    int16 size_key, size_text;
+    
+    key = "thisiskey";
+    size_key = strlen(key);
+
+    text = "This is private text";
+    size_text = strlen(text);
+
+    printf("INITIALIZING..."); F;
+    rc4 = rc4init(key, size_key);
+    printf("done\n");
+
+    printf("'%s'\n ->", text); F;
+    encrypted = rc4encrypt(text, size_text);
+    printf("done\n");
+
+    return 0;
+}
